854796063fb963fb1fb33a8584fa96732b9973d2
alfalfaw <limuyle@163.com>
2020-06-14 15:26:01 +0800

    fixed upload url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -67,7 +67,8 @@ module.exports = (app) => {
   // 该中间件将文件赋值到req.file
   app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
     const file = req.file
-    file.url = `http://localhost:3000/uploads/${file.filename}`
+    // file.url = `http://localhost:3000/uploads/${file.filename}`
+    file.url = `https://moba.alfalfa.website:3000/uploads/${file.filename}`
     res.send(file)
   })
 
